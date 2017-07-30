6b122fffa5fe2a6b448d0636a0d036b02d164b6c
Alfred Wu <awu123@gmail.com>
2017-07-30 16:17:25 -0400

    Initial commit.

---
+++ Universal Movie Database/Movie.swift
@@ -0,0 +1,9 @@
+//
+//  Movie.swift
+//  Universal Movie Database
+//
+//  Created by Alfred Wu on 7/30/17.
+//  Copyright © 2017 Alfred Wu. All rights reserved.
+//
+
+import Foundation
--- Universal Movie Database/ViewController.swift
+++ Universal Movie Database/ViewController.swift
@@ -8,12 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+
+class ViewController: UIViewController, XMLParserDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let bundle = Bundle.main
+        
+        //let path = bundle.path(forResource: "movies", ofType: "xml")
+        //let parser: XMLParser = XMLParser(contentsOf: URL(fileURLWithPath: path!))!
+
+        if let url = bundle.url(forResource: "movies", withExtension: "xml") {
+            if let parser: XMLParser = XMLParser(contentsOf: url) {
+                parser.delegate = self
+                parser.parse()
+            }
+        }
+        
     }
+    
+    func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
+        
+        
+        switch elementName {
+            
+        case "movie":
+            print(attributeDict["Title"] ?? "Title is missing")
+            print(attributeDict["Rating"] ?? "Rating is missing")
+        default:
+            break
+        }
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
